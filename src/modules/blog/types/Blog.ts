3dab3015eaac5d66e3b477e28780277f46053e71
Luiz Humberto <luizhumberto@gmail.com>
2023-11-20 21:11:23 -0300

    feat: create blog type

---
+++ src/modules/blog/types/Blog.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+import { toTypedSchema } from '@vee-validate/zod'
+
+export const PostSchema = z.object({
+  id: z.string(),
+  title: z.string().min(1, 'Campo obrigatório').max(100, 'Máximo 100 letras'),
+  text: z.string().min(1, 'Campo obrigatório').max(500, 'Máximo 100 letras'),
+  created_at: z.string().nullish(),
+  updated_at: z.string().nullish(),
+})
+
+export const validationPostSchema = toTypedSchema(PostSchema)
+
+export type Post = z.infer<typeof PostSchema>
