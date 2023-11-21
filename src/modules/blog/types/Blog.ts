5aac9dfa3b90a324b3c9eb0c8a7fb2e576995cf7
betogroo <luizhumberto@gmail.com>
2023-11-21 10:31:39 -0300

    fix: max text lenght error msg

--- src/modules/blog/types/Blog.ts
+++ src/modules/blog/types/Blog.ts
@@ -4,7 +4,7 @@ import { toTypedSchema } from '@vee-validate/zod'
 export const PostSchema = z.object({
   id: z.number().nullish(),
   title: z.string().min(1, 'Campo obrigatório').max(100, 'Máximo 100 letras'),
-  text: z.string().min(1, 'Campo obrigatório').max(500, 'Máximo 100 letras'),
+  text: z.string().min(1, 'Campo obrigatório').max(500, 'Máximo 500 letras'),
   created_at: z.string().nullish(),
 })
 
