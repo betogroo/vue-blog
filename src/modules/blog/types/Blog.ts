322a7f9e0a63ec098e278cbc425649b164a58968
betogroo <luizhumberto@gmail.com>
2023-11-21 10:17:14 -0300

    fix: blog types

--- src/modules/blog/types/Blog.ts
+++ src/modules/blog/types/Blog.ts
@@ -2,11 +2,10 @@ import { z } from 'zod'
 import { toTypedSchema } from '@vee-validate/zod'
 
 export const PostSchema = z.object({
-  id: z.string().default(''),
+  id: z.number().nullish(),
   title: z.string().min(1, 'Campo obrigatório').max(100, 'Máximo 100 letras'),
   text: z.string().min(1, 'Campo obrigatório').max(500, 'Máximo 100 letras'),
   created_at: z.string().nullish(),
-  updated_at: z.string().nullish(),
 })
 
 export const validationPostSchema = toTypedSchema(PostSchema)
