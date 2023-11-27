ac7a7a09ba7e964985f52ef0366f9c3476f8b142
betogroo <luizhumberto@gmail.com>
2023-11-27 16:59:53 -0300

    refactor: alter max characters in text to 5000

--- src/modules/blog/types/Blog.ts
+++ src/modules/blog/types/Blog.ts
@@ -5,7 +5,7 @@ import { ProfileSchema } from '@/modules/auth/types/Profile'
 export const PostSchema = z.object({
   id: z.number().nullish(),
   title: z.string().min(1, 'Campo obrigatório').max(100, 'Máximo 100 letras'),
-  text: z.string().min(1, 'Campo obrigatório').max(2000, 'Máximo 2000 letras'),
+  text: z.string().min(1, 'Campo obrigatório').max(5000, 'Máximo 5000 letras'),
   created_at: z.string().nullish(),
   user_id: z.string().nullish(),
 })
