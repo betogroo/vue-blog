77648aca5cb2b1615cecea633ff5656b5b9e923a
betogroo <luizhumberto@gmail.com>
2023-11-27 13:39:21 -0300

    feat: create useText shared Composable

---
+++ src/shared/composables/useText.ts
@@ -0,0 +1,10 @@
+const useText = () => {
+  const limitText = (text: string, limit: number): string => {
+    const limitedText = ''
+    console.log(text, limit)
+    return limitedText
+  }
+  return { limitText }
+}
+
+export default useText
