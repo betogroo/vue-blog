7d27366b528dc7af775c28d0b76efc4ad17e1123
Luiz Humberto <luizhumberto@gmail.com>
2023-12-07 17:42:15 -0300

    refactor(temp): edit and toggle dialog actions

--- src/modules/blog/views/BlogHome.vue
+++ src/modules/blog/views/BlogHome.vue
@@ -4,10 +4,12 @@ import { usePost } from '../composables'
 import { ref } from 'vue'
 import { useBlogStore } from '../store/useBlogStore'
 import { useProfileStore } from '@/modules/auth/store/useProfileStore'
-import AppDialogFull from '@/shared/components/app/AppDialogFull.vue'
-import { Post } from '../types/Blog'
+import { AppDialogFull, BtnOrIcon } from '@/shared/components'
+import { type Post } from '../types/Blog'
 
+//reactive
 const indexPending = ref<number | string>(-1)
+const dialog = ref(false)
 
 // composable
 const {
@@ -17,11 +19,14 @@ const {
   addPost,
   isPending: postPending,
   posts,
+  post: _post,
+  getPost,
 } = usePost()
 
 // store
 const blogStore = useBlogStore()
 const profileStore = useProfileStore()
+const dialogAction = ref<string>('')
 
 const deletePost = async (id: number | string) => {
   indexPending.value = posts.value.findIndex((item) => item.id === id)
@@ -36,10 +41,10 @@ const deletePost = async (id: number | string) => {
   }
 }
 
-const editPost = async (id: number | string) => {
-  indexPending.value = posts.value.findIndex((item) => item.id === id)
-  await _editPost(+id)
-  indexPending.value = -1
+const toggleEditPost = async (id: number) => {
+  toggleDialog('editPost')
+  await getPost(id)
+  console.log(_post.value)
 }
 
 const submitPost = async (post: Post) => {
@@ -49,14 +54,14 @@ const submitPost = async (post: Post) => {
     const data = await addPost(post, user_id)
     if (!data) throw Error('Não foi possível postar.')
     await fetchPosts()
-    toggleDialog()
+    toggleDialog('')
   } catch (err) {
     console.log('BlogHome', err)
   }
 }
 
-const dialog = ref(false)
-const toggleDialog = () => {
+const toggleDialog = (action: string) => {
+  dialogAction.value = action
   dialog.value = !dialog.value
 }
 
@@ -68,17 +73,11 @@ await fetchPosts()
     <v-card
       class="text-right px-3"
       variant="text"
-    >
-      <AppDialogFull
-        v-model="dialog"
-        button-color="success"
-        button-title="Novo Post"
-      >
-        <PostForm
-          :is-pending="postPending"
-          @submit-post="(post) => submitPost(post)"
-        />
-      </AppDialogFull>
+      ><BtnOrIcon
+        icon="mdi-plus"
+        text="Novo Post"
+        @handle-click="toggleDialog('submitPost')"
+      />
     </v-card>
     <PostCard
       v-for="(post, i) in blogStore.posts"
@@ -88,7 +87,22 @@ await fetchPosts()
       :post="post"
       :user_id="profileStore.userProfile.id"
       @handle-delete="(id) => deletePost(id)"
-      @handle-edit="(id) => editPost(id)"
+      @handle-edit="(id) => toggleEditPost(+id)"
     />
   </v-container>
+  <AppDialogFull
+    v-model="dialog"
+    :title="dialogAction === 'submitPost' ? 'Novo Post' : 'Editar post'"
+  >
+    <PostForm
+      v-if="dialogAction === 'submitPost'"
+      :is-pending="postPending"
+      @submit-post="(post) => submitPost(post)"
+    />
+    <PostForm
+      v-else
+      :is-pending="postPending"
+      :post="_post"
+    />
+  </AppDialogFull>
 </template>
