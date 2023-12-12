012ee7eeb60c7b71b3b69a771f21c0bca62f6154
Luiz Humberto <luizhumberto@gmail.com>
2023-12-12 15:13:42 -0300

    feat: create post state and reset

--- src/modules/blog/store/useBlogStore.ts
+++ src/modules/blog/store/useBlogStore.ts
@@ -4,15 +4,19 @@ import { PostWithProfile, CommentWithProfile } from '../types/Blog'
 import { ref } from 'vue'
 
 export const useBlogStore = defineStore('blog', () => {
+  const post = ref<PostWithProfile>()
   const posts = ref<PostWithProfile[]>([])
   const comments = ref<CommentWithProfile[]>([])
 
   const $resetPosts = () => {
     posts.value = []
   }
+  const $resetPost = () => {
+    post.value = undefined
+  }
   const $resetComments = () => {
     comments.value = []
   }
 
-  return { posts, comments, $resetPosts, $resetComments }
+  return { post, posts, comments, $resetPosts, $resetComments, $resetPost }
 })
